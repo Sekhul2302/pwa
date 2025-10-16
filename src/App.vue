9f74f641162b576df218bccc1166056f6ff879b2
Sekhul Amin <samin0223@pnm.co.id>
2025-10-16 09:51:14 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("✅ Service Worker registered"))
+      .catch((err) => console.log("SW registration failed:", err));
+  });
+}
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
